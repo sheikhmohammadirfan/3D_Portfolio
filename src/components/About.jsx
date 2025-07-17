5c6660f21a917a227089e6a5ed41d8f70faf2200
Mohammad Irfan <sheikhirfansc76@gmail.con>
2025-07-18 05:19:48 +0530

    Fix: replace react-tilt with react-parallax-tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,6 @@
 // src/components/About.jsx
 import React from 'react';
-// import { Tilt } from 'react-tilt';
-import Tilt from "react-parallax-tilt";
-
+import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -10,14 +8,13 @@ import { services } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const tiltOptions = {
-  max: 45,
-  scale: 1,
-  speed: 450,
-};
-
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt options={tiltOptions} className="xs:w-[250px] w-full">
+  <Tilt
+    glareEnable={false}
+    scale={1}
+    transitionSpeed={450}
+    className="xs:w-[250px] w-full"
+  >
     <motion.div
       variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -41,8 +38,8 @@ const About = () => (
       variants={fadeIn('', '', 0.1, 1)}
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
     >
-       I’m a Frontend Developer with a Master’s degree in Computer Science, skilled in building responsive, user-focused web applications using React, Node.js, JavaScript, and modern UI frameworks like Tailwind CSS and Bootstrap.
-       I specialize in translating Figma designs into clean, scalable code, optimizing performance across devices, and crafting intuitive interfaces that elevate user experience. With a strong grasp of cross-browser compatibility, semantic HTML, and component-based architecture, I bring both technical precision and design sensibility to every project.
+      I’m a Frontend Developer with a Master’s degree in Computer Science, skilled in building responsive, user-focused web applications using React, Node.js, JavaScript, and modern UI frameworks like Tailwind CSS and Bootstrap.
+      I specialize in translating Figma designs into clean, scalable code, optimizing performance across devices, and crafting intuitive interfaces that elevate user experience. With a strong grasp of cross-browser compatibility, semantic HTML, and component-based architecture, I bring both technical precision and design sensibility to every project.
     </motion.p>
 
     <div className="mt-20 flex flex-wrap gap-10">
